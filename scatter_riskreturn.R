6cedc00e93857675e2a3f5c6ffe83c1224f596fc
timelyportfolio <kent.russell@timelyportfolio.com>
2013-04-24 14:12:20 -0500

    weirdness in scatterChart model seem to be bugs outside of rCharts

--- scatter_riskreturn.R
+++ scatter_riskreturn.R
@@ -8,12 +8,17 @@ data(managers)
 data <- t(table.AnnualizedReturns(managers))
 data <- data.frame(rownames(data),data, row.names=NULL)
 
-colnames(data)[1] <- "Series"
+colnames(data)[1] <- "series"
+colnames(data) <- c("series","y","x")
+
+#add a size variable for instance pct of assets in a portfolio context
+#data$size <- data$x
+
 #remove . from column names
 colnames(data) <- sapply(colnames(data), gsub, pattern="\\.", replacement="")
 
-p2 <- nvd3Plot(AnnualizedReturn ~ AnnualizedStdDev,
-               group = "Series",
+p2 <- nvd3Plot(y ~ x,
+               group = "series",
                data = data,
                type = "scatterChart")
 p2$chart(showDistX = TRUE)
